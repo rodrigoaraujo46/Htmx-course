446840fd4573d1090d327992ef24561c741bdf00
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2024-11-11 02:27:30 +0000

    Changed from a count button to a form that inserts contacts to be displayed

--- cmd/main.go
+++ cmd/main.go
@@ -22,8 +22,62 @@ func newTemplate() *Templates {
 	}
 }
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name string
+	Email string
+}
+
+func newContact(name, email string) *Contact {
+	return &Contact {
+		Name: name,
+		Email: email,
+	}
+}
+
+type Contacts = map[string]*Contact
+
+type ContactData struct {
+	Contacts Contacts
+}
+
+func newContactData() *ContactData {
+	return &ContactData{
+		Contacts: map[string]*Contact{
+			"john@example.com": newContact("John", "john@example.com"),
+			"joe@example.com":  newContact("Joe", "joe@example.com"),
+			"king@example.com": newContact("King", "king@example.com"),
+		},
+	}
+}
+
+func (contactData *ContactData) hasEmail(email string) bool {
+	_, exists := contactData.Contacts[email]
+	if exists {
+		return true
+	}
+	return false
+}
+
+type FormData = map[string]string
+
+func newFormData() FormData{
+	return FormData{
+			"errorMessage" : "",
+			"name" : "",
+			"email" : "",
+		}
+}
+
+type PageData struct{
+	ContactData ContactData
+	FormData FormData
+}
+
+func newPageData() PageData{
+	return PageData{
+		ContactData: *newContactData(),
+		FormData: newFormData(),
+	}
 }
 
 func main()  {
@@ -32,16 +86,28 @@ func main()  {
 	e.Use(middleware.Logger())
 
 	e.Renderer = newTemplate()
-
-	count := Count { Count : 0}
+	pageData := newPageData()
 
 	e.GET("/", func (c echo.Context) error {
-		return c.Render(200, "index.html", count)
+		return c.Render(200, "index", pageData)
 	})
+	e.POST("/contacts", func (c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
 
-	e.POST("/count", func (c echo.Context) error {
-		count.Count++
-		return c.Render(200, "index.html", count)
+		if pageData.ContactData.hasEmail(email){
+			pageData.FormData["name"] = name
+			pageData.FormData["email"] = email
+			pageData.FormData["errorMessage"] = "Try a different email"
+
+			return c.Render(422, "createContact", pageData.FormData)
+		}
+		contact := newContact(name, email)
+		pageData.ContactData.Contacts[email] = contact
+
+		c.Render(200,"createContact",newFormData())
+
+		return c.Render(200, "oob-contact", contact)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
