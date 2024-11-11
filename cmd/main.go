96028b6c83d425c0437a59a181ec93a8fbf3ed3d
rodrigoaraujo46 <rodrigo46.dev@gmail.com>
2024-11-11 09:19:12 +0000

    Finished Primes htmx course

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"io"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -22,15 +23,19 @@ func newTemplate() *Templates {
 	}
 }
 
+var id = 0
 type Contact struct {
+	Id int
 	Name string
 	Email string
 }
 
 func newContact(name, email string) *Contact {
+	id++
 	return &Contact {
 		Name: name,
 		Email: email,
+		Id: id,
 	}
 }
 
@@ -43,8 +48,8 @@ type ContactData struct {
 func newContactData() *ContactData {
 	return &ContactData{
 		Contacts: map[string]*Contact{
-			"john@example.com": newContact("John", "john@example.com"),
-			"joe@example.com":  newContact("Joe", "joe@example.com"),
+			"john@example.com" : newContact("John", "john@example.com"),
+			"joe@example.com" :  newContact("Joe", "joe@example.com"),
 			"king@example.com": newContact("King", "king@example.com"),
 		},
 	}
@@ -62,9 +67,9 @@ type FormData = map[string]string
 
 func newFormData() FormData{
 	return FormData{
-			"errorMessage" : "",
-			"name" : "",
-			"email" : "",
+			"ErrorMessage" : "",
+			"Name" : "",
+			"Email" : "",
 		}
 }
 
@@ -81,9 +86,12 @@ func newPageData() PageData{
 }
 
 func main()  {
-
 	e := echo.New()
 	e.Use(middleware.Logger())
+
+	e.File("/favicon.ico", "images/favicon.ico")
+	e.Static("/images", "images")
+	e.Static("/css", "css")
 
 	e.Renderer = newTemplate()
 	pageData := newPageData()
@@ -91,23 +99,40 @@ func main()  {
 	e.GET("/", func (c echo.Context) error {
 		return c.Render(200, "index", pageData)
 	})
+
+	e.GET("/favicon.ico", func(c echo.Context) error {
+		return c.File("images/favicon.ico")
+	})
+
 	e.POST("/contacts", func (c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
 
 		if pageData.ContactData.hasEmail(email){
-			pageData.FormData["name"] = name
-			pageData.FormData["email"] = email
-			pageData.FormData["errorMessage"] = "Try a different email"
+			pageData.FormData["Name"] = name
+			pageData.FormData["Email"] = email
+			pageData.FormData["ErrorMessage"] = "Try a different email"
 
 			return c.Render(422, "createContact", pageData.FormData)
 		}
+
 		contact := newContact(name, email)
 		pageData.ContactData.Contacts[email] = contact
 
 		c.Render(200,"createContact",newFormData())
 
 		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:email", func(c echo.Context) error{
+		time.Sleep(3*time.Second)
+		email := c.Param("email")
+
+		if !pageData.ContactData.hasEmail(email){
+			return c.NoContent(200)
+		}
+		delete(pageData.ContactData.Contacts, email)
+		return c.NoContent(200)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
